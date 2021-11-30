ad7ba33c18fa4af790d5ca37172584169a06c10b
justinrivera47 <justinrivera47@gmail.com>
2021-11-30 16:55:58 -0500

    Completed mouse over.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//Nav links
+const navLinks = document.querySelector('nav')
+
+navLinks.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.color = "red";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 1000);
+}, false);
+
+//Intro Image 
+const introImg = document.querySelector('.intro img')
+
+//All H2
+const h2 = document.querySelectorAll('h2');
+
+//btn
+const btn = document.querySelectorAll('.btn');
+
+//h1
+const h1 = document.querySelector('h1');
